274e2e7ea95a072ea986dcb4c55ba2deab2b690f
Leif Niemczik <leif.niem@gmail.com>
2022-02-17 15:36:22 +0100

    neue texte, fixes

--- src/_data/projekte.js
+++ src/_data/projekte.js
@@ -1,3 +1,4 @@
 const absolventinnen = require("./absolventinnen")
+let projekte = [...absolventinnen]
 
-module.exports = absolventinnen.sort((a, b) => a.title < b.title ? -1 : 1)
+module.exports = projekte.sort((a, b) => a.title < b.title ? -1 : 1)
